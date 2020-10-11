52659a25f5917df6350142f18f6435f01ec764fb
ivan-holeshau <goleshov85@gmail.com>
2020-10-11 19:45:02 +0300

    featur(): TAPMR-2
    add bigViewNews

--- src/core/reducer/mainReducer.ts
+++ src/core/reducer/mainReducer.ts
@@ -1,12 +1,26 @@
 import {} from 'redux'
 import {AVTORISATION} from '../constants/actions'
 import {Action} from './index'
-
+import {BigViewNews} from './index'
  interface MainState{
     registered: boolean,
+    bigViewNews:BigViewNews,
 }
 export const initalState:MainState = {
  registered:false,
+ bigViewNews:{
+     text:`Когда нужно отдохнуть от беллетристики, но 
+     в то же время хочется чего-то легкого - ищи 
+     спасение в документальной прозе.
+     Помощь`,
+     title:`Парам:«Русский дневник» Джон Стей
+     нбек ляля`,
+     id:1,
+    alt:'picture',
+     image:'1.png',
+     readTime:'#читаем  - 3 мин чит.'
+
+ }
 }
 
 
--- src/core/selector/mainSelector.ts
+++ src/core/selector/mainSelector.ts
@@ -3,4 +3,6 @@ import {RootState} from '../reducer'
 
 
 export const isRegistered = (state:RootState):boolean => state.mainReducer.registered;
+
+export const bigNews = (state:RootState) => state.mainReducer.bigViewNews;
             
